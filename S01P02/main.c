9ddb98e8ac249484243897d2b8d0058a02d9c253
Alex <carbl@yandex.ru>
2023-10-28 17:16:53 +0300

    S01P02 Let's

---
+++ S01P02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Let\'s\n");
+	printf("   go\n");
+	printf("    to walk\n\n");
+	printf("Let\'s\n");
+	printf("\tgo\n");
+	printf("\t\tto walk\n");
+	
+	return 0;
+}
+
