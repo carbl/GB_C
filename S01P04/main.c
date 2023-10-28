baa449b4f9aa0cb0517391c358a0f7703c2520f0
Alex <carbl@yandex.ru>
2023-10-28 17:33:34 +0300

    S01P04 \_/

---
+++ S01P04/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf(" _   _\n");
+	printf("\x2F \\_\x2F \\\n");
+	printf("\\_\x2F \\_\x2F\n");
+	printf("\x2F \\_\x2F \\\n");
+	printf("\\_\x2F \\_\x2F\n");
+	
+	return 0;
+}
+
